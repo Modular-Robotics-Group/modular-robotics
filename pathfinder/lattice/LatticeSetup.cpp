099e4c11e533f8460c7b98d76df831e61799c128
clementsaic <andrew.clements.504@gmail.com>
2024-11-20 04:54:24 -0500

    [Pathfinder] Remove call to test function

--- pathfinder/lattice/LatticeSetup.cpp
+++ pathfinder/lattice/LatticeSetup.cpp
@@ -22,7 +22,6 @@ namespace LatticeSetup {
         }
         nlohmann::json j;
         file >> j;
-        ModuleProperties::CallFunction("PropertyFuncTest");
         std::cout << "\tCreating Lattice...   ";
         if (j.contains("tensorPadding")) {
             Lattice::InitLattice(j["order"], j["axisSize"], j["tensorPadding"]);
