1e799f2256a8b9ce3624479d5ff0dc0c3f6a2013
clementsaic <andrew.clements.504@gmail.com>
2024-11-25 22:00:36 -0500

    [Pathfinder] Fix incorrect colors being exported to .scen files
    Funnily enough this didn't actually affect any of the files made through 3DTiler which is why it went so long unnoticed

--- pathfinder/utility/color_util.cpp
+++ pathfinder/utility/color_util.cpp
@@ -4,8 +4,8 @@ namespace Colors {
     ColorsRGB::ColorsRGB(const int r, const int g, const int b) : red(r), green(g), blue(b) {}
 
     ColorsRGB::ColorsRGB(const int rgbInt) {
-        red = rgbInt & 0xFF0000;
-        green = rgbInt & 0x00FF00;
+        red = (rgbInt & 0xFF0000) >> 16;
+        green = (rgbInt & 0x00FF00) >> 8;
         blue = rgbInt & 0x0000FF;
     }
 
