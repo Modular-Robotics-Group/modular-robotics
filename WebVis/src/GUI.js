7f1e6c5459637f0a863557ee743c4596fa81aa64
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2025-03-10 14:01:24 -0400

    Format opacity settings for improved readability

--- WebVis/src/GUI.js
+++ WebVis/src/GUI.js
@@ -23,9 +23,9 @@ let EXAMPLE_SCENARIOS = [
 
 // Opacity settings for changing layers / visualizing adjacent layers
 const OPACITY_SETTINGS = Object.freeze({
-    FULLY_OPAQUE: 1.0,
-    ADJACENT_SLICE: 0.4,
-    TRANSPARENT: 0.0
+    FULLY_OPAQUE:       1.0,
+    ADJACENT_SLICE:     0.4,
+    TRANSPARENT:        0.0
 });
 
 const LAYER_SETTINGS = Object.freeze({
