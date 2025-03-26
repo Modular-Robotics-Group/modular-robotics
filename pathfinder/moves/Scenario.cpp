0b8a0109b17be4c087802a6b8880818c5f6f394e
clementsaic <56006704+Clementsaic@users.noreply.github.com>
2025-03-26 02:37:03 -0400

    [Pathfinder] More file tweaking

--- pathfinder/moves/Scenario.cpp
+++ pathfinder/moves/Scenario.cpp
@@ -32,7 +32,8 @@ std::string Scenario::TryGetScenDesc(const std::string& initialFile) {
 }
 
 void Scenario::ExportToScenFile(const std::vector<const Configuration*>& path, const ScenInfo& scenInfo) {
-    std::ofstream file(scenInfo.exportFile);
+    std::ofstream file;
+    file.open(scenInfo.exportFile);
     if (!file.is_open()) {
         std::cerr << "Unable to open file " << scenInfo.exportFile << std::endl;
         throw std::ios_base::failure("Unable to open file " + scenInfo.exportFile + "\n");
